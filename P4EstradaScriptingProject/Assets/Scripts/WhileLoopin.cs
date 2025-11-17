d8ef95a9f517bbb4997770150010445f040c5db3
novel6464 <764297@student.dusd.net>
2025-11-17 10:57:46 -0800

    mew
    mew

---
+++ P4EstradaScriptingProject/Assets/Scripts/WhileLoopin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoopin : MonoBehaviour
+{
+    int cupsInTheSink = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            while(cupsInTheSink > 0)
+            {
+                Debug.Log("I've washed a cup");
+                cupsInTheSink--;
+            }
+    }
+
+    
+}
