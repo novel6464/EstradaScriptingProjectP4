f31677834d8fc49f0b6f019db2f561a32c181497
novel6464 <764297@student.dusd.net>
2026-02-10 10:26:26 -0800

    meow
    meow

---
+++ P4EstradaScriptingProject/Assets/Scripts/DestroyBasic.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBasic : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.J))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
