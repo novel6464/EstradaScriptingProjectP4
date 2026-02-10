85292df538460061364a7f730dde343d493d19ea
novel6464 <764297@student.dusd.net>
2026-02-10 10:27:15 -0800

    meow
    meow

--- P4EstradaScriptingProject/Assets/Scripts/DestroyComponent.cs
+++ P4EstradaScriptingProject/Assets/Scripts/DestroyComponent.cs
@@ -9,7 +9,7 @@ public class DestroyComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.A))
         {
             Destroy(GetComponent<MeshRenderer>());
         }
