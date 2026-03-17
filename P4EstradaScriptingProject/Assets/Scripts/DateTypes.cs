24b84ec400c4b3bccf7202dc7fe7cf3f9aeff47a
novel6464 <764297@student.dusd.net>
2026-03-17 10:23:12 -0700

    dsfsdfsdf
    sdfsdfsd

--- P4EstradaScriptingProject/Assets/Scripts/DateTypes.cs
+++ P4EstradaScriptingProject/Assets/Scripts/DateTypes.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeltaTime : MonoBehaviour
+public class DateTypes : MonoBehaviour
 {
-    public float speed = 5f;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 pos = transform.position;
+        pos = new Vector3(0, 2,0);
+
+
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.foward * (speed * Time.deltaTime);
+        
     }
 }
