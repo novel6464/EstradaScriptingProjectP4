e919cdd6a652a08df9e3be0724a036cdfc53eee0
novel6464 <764297@student.dusd.net>
2026-03-25 11:03:28 -0700

    sadfsawdrfasdrf
    asdffsdgfdssdfafdsa

---
+++ P4EstradaScriptingProject/Assets/Scripts/Inventory.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;    
+        public int rockets;
+        public float fuel;
+
+
+        public Stuff(int bul, int gren, int roc)
+        {
+            bullets = bul;
+            grenades = gren;
+            rockets = roc;
+            
+        }
+        public Stuff(int bul, float fu)
+        {
+            bullets = bul;
+            fuel = fu;
+        }
+
+        public Stuff()
+        {
+            bullets = 1;
+            grenades = 1;
+            rockets = 1;
+
+            
+        }
+    }
+    public Stuff myStuff = new Stuff(50, 5, 5);
+
+    public Stuff myOtherStufff = new Stuff(50, 1.5f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(myStuff.bullets);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
